70344f02bf0ee63df374e5ea0ce2bba0302221a0
Eonist <30n1st@gmail.com>
2019-11-25 12:02:56 +0100

    Bump deps

---
+++ GeomKitExampleMacOS/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GeomKitExampleMacOS
+//
+//  Created by Eon on 25/11/2019.
+//  Copyright © 2019 FutureLab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+   @IBOutlet weak var window: NSWindow!
+
+
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+   }
+}
