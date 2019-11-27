1b5a7a474334c8b4d9dfff588cb305d8cfe216d0
Eonist <30n1st@gmail.com>
2019-11-27 11:13:25 +0100

    Bump specs

--- GeomKitTest/GeomKitTest.swift
+++ GeomKitTest/GeomKitTest.swift
@@ -13,19 +13,7 @@ class GeomKitTest: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-      _ = { // Interpolation
-         let result = CGPointParser.interpolate(a: .zero, b: .init(x: 10, y: 10), scalar: 0.5)
-         Swift.print("result:  \(result)")
-         XCTAssertEqual(result, CGPoint(x: 5, y: 5))
-      }()
-      _ = { // Difference
-         XCTAssertEqual(CGPointParser.difference(p1: CGPoint(x: 20, y: 20), p2: CGPoint(x: -40, y: -40)), CGPoint(x: -60, y: -60))//Output:
-         XCTAssertEqual(CGPointParser.difference(p1: CGPoint(x: 20, y: 20), p2: CGPoint(x: 40, y: -40)), CGPoint(x: 20, y: -60))//Output:
-         XCTAssertEqual(CGPointParser.difference(p1: CGPoint(x: 2, y: 2), p2: CGPoint(x: 4, y: 4)), CGPoint(x: 2, y: 2))//Outputs:
-         XCTAssertEqual(CGPointParser.difference(p1: CGPoint(x: 2, y: 2), p2: CGPoint(x: -4, y: -4)), CGPoint(x: -6, y: -6))//Outputs:
-         XCTAssertEqual(CGPointParser.difference(p1: CGPoint(x: -2, y: -2), p2: CGPoint(x: -4, y: -4)), CGPoint(x: -2, y: -2))//Outputs:
-         XCTAssertEqual(CGPointParser.difference(p1: CGPoint(x: -2, y: -2), p2: CGPoint(x: 4, y: 4)), CGPoint(x: 6, y: 6))//Outputs:
-      }()
+     
     }
 
     func testPerformanceExample() {
